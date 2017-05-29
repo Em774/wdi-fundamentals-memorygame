e754206f3a816554dd22d2642b78f899995cff41
Emilie Dussaix <Emilie@emilies-mbp.home>
2017-05-29 23:15:44 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
 var cards = ["Queen", "Queen", "King", "King"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push('cardOne');
-console.log("User flipped Queen");
+var checkForMatch = function () {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+      		alert("You found a match!");
+      	}
+  		else {
+      		alert("Sorry, try again.");
+  		}
+	};
+};
 
-var cardTwo = cards[2];
-cardsInPlay.push('cardTwo');
-console.log("User flipped King");
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert('You found a match!');
-} else {
-	alert('Sorry, try again.');
-}
+checkForMatch();
+
+};
+
+flipCard(0);
+flipCard(2);
 
