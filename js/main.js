9c01f528bc08af26017c87f21db8044a0ec19715
Emilie Dussaix <Emilie@emilies-mbp.home>
2017-06-04 16:47:41 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
-var cards = ["Queen", "Queen", "King", "King"];
+var cards = [
+	{
+		rank: "Queen",
+		suit: "Hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "Queen",
+		suit: "Diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "King",
+		suit: "Hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "King",
+		suit: "Diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
+
+
 var cardsInPlay = [];
 
 var checkForMatch = function () {
@@ -13,8 +36,10 @@ var checkForMatch = function () {
 };
 
 var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 
 checkForMatch();
 
