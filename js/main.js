1da1782d3e71510cfde832dd278c5f8e34bf8a49
Emilie Dussaix <Emilie@emilies-mbp.home>
2017-06-05 11:55:31 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ var cards = [
 ];
 
 
+
 var cardsInPlay = [];
 
 var checkForMatch = function () {
@@ -32,19 +33,30 @@ var checkForMatch = function () {
   		else {
       		alert("Sorry, try again.");
   		}
-	};
+	}
 };
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
 
 checkForMatch();
 
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src', 'images/back.png');
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
 
